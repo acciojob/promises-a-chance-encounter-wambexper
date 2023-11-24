35d75baef14e23033d4cd817f3cc999c90b48cf7
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-24 09:39:32 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,28 @@
-// Function to generate a random number between 1 and 10
-  function getRandomNumber() {
-    return Math.floor(Math.random() * 10) + 1;
-  }
+window.promises = [];
+const p1 = new Promise((res, rej)=>{
+	setTimeout(res, 100, "p1");
+});
+const p2 = new Promise((res, rej)=>{
+	setTimeout(res, 200, "p2");
+});
+const p3 = new Promise((res, rej)=>{
+	setTimeout(res, 300, "p3");
+});
+const p4 = new Promise((res, rej)=>{
+	setTimeout(res, 400, "p4");
+});
+const p5 = new Promise((res, rej)=>{
+	setTimeout(res, 500, "p5");
+});
+promises.push(p1);
+promises.push(p2);
+promises.push(p3);
+promises.push(p4);
+promises.push(p5);
 
-  // Function to create a promise that resolves to a random number or rejects with an error
-  function createPromise() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (Math.random() < 0.5) {
-          resolve(getRandomNumber());
-        } else {
-          reject(new Error('Promise rejected with error'));
-        }
-      }, 1000);
-    });
-  }
+Promise.any(promises).then((data)=>{
+	document.getElementById("output").innerText = data;
+});
 
-  // Create an array of 5 promises
-  const promises = [
-    createPromise(),
-    createPromise(),
-    createPromise(),
-    createPromise(),
-    createPromise()
-  ];
-
-  // Use Promise.all to wait for all promises to settle and log the results or errors
-  Promise.all(promises)
-    .then(results => {
-      results.forEach((result, index) => {
-        if (result instanceof Error) {
-          console.log(`Promise ${index + 1} rejected with error`);
-        } else {
-          const outputDiv = document.getElementById('output');
-          const p = document.createElement('p');
-          p.textContent = `Promise ${index + 1}: ${result}`;
-          outputDiv.appendChild(p);
-        }
-      });
-    })
-    .catch(error => {
-      console.log('Error:', error);
-    });
+// Do not change the code above this
+// add your promises to the array `promises`
